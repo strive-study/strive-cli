f43cec995bbf29a919ab0e59bc33febeaf06927f
xiaoli <2585960098@qq.com>
2023-07-14 01:03:08 +0800

    feat:依赖合并算法diff查找

--- commands/add/lib/index.js.js
+++ commands/add/lib/index.js.js
@@ -1,8 +1,12 @@
 const path = require('path')
+const fs = require('fs')
 const { homedir } = require('os')
 const inquirer = require('inquirer')
 const pathExists = require('path-exists')
 const fse = require('fs-extra')
+const { glob } = require('glob')
+const ejs = require('ejs')
+const pkgUp = require('pkg-up')
 const Command = require('@strive-cli/command')
 const Package = require('@strive-cli/package')
 const { sleep, spinnerStart } = require('@strive-cli/utils')
@@ -12,7 +16,8 @@ const PAGE_TEMPLATE = [
     name: 'Vue3首页模板',
     npmName: 'strive-cli-template-page-vue3',
     version: 'latest',
-    targetPath: 'src/views/Home'
+    targetPath: 'src/views/Home',
+    ignore: ['assets/**']
   }
 ]
 class AddCommand extends Command {
@@ -31,6 +36,7 @@ class AddCommand extends Command {
   }
 
   async installTemplate() {
+    log.info('开始安装页面模板')
     const templatePath = path.resolve(
       this.pageTemplatePkg.cacheFilePath,
       'template',
@@ -39,12 +45,86 @@ class AddCommand extends Command {
     if (!(await pathExists(templatePath))) {
       throw new Error('页面模板不存在')
     }
-    const targetPath = this.targetPath
+    const targetPath = this.targetPath // 复制目标路径
     log.verbose('templatePath', templatePath)
     log.verbose('targetPath', targetPath)
     fse.ensureDirSync(templatePath)
     fse.ensureDirSync(targetPath)
     fse.copySync(templatePath, targetPath)
+    // ejs渲染
+    await this.ejsRender({ ignore: this.pageTemplate.ignore, targetPath })
+    await this.dependenciesMerge({ targetPath, templatePath })
+    log.success('安装页面模板成功')
+  }
+
+  async ejsRender({ ignore, targetPath }) {
+    try {
+      const files = await glob('**', {
+        cwd: targetPath,
+        ignore,
+        nodir: true
+      })
+      return Promise.all(
+        files.map(file => {
+          const filePath = path.join(targetPath, file)
+          return new Promise((resolve, reject) => {
+            ejs.renderFile(
+              filePath,
+              { name: this.pageTemplate.pageName },
+              (err, res) => {
+                if (err) {
+                  reject(err)
+                } else {
+                  fs.writeFileSync(filePath, res)
+                  resolve(res)
+                }
+              }
+            )
+          })
+        })
+      ).catch(e => log.error(e.message))
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+
+  async dependenciesMerge({ targetPath, templatePath }) {
+    const templatePkgPath = pkgUp.sync({ cwd: templatePath })
+    const targetPkgPath = pkgUp.sync({ cwd: targetPath })
+    const templateDep = fse.readJSONSync(templatePkgPath).dependencies || {}
+    const targetDep = fse.readJSONSync(targetPkgPath).dependencies || {}
+    // 比较两个依赖差别
+    const templateDepArr = obj2Array(templateDep)
+    const targetDepArr = obj2Array(targetDep)
+    const finalDep = depDiff(templateDepArr, targetDepArr)
+    console.log('finalDep', finalDep)
+    function obj2Array(o) {
+      const arr = []
+      Object.keys(o).forEach(key => {
+        arr.push({
+          key,
+          value: o[key]
+        })
+      })
+      return arr
+    }
+    function depDiff(templateDepArr, targetDepArr) {
+      let finalDep = [...targetDepArr]
+      // 模板中存在依赖，项目中不存在
+      // 模板中存在依赖，项目中也存在，不拷贝但提示用户
+      templateDepArr.forEach(templateDep => {
+        const duplicatedDep = targetDepArr.find(
+          targetDep => targetDep.key === templateDep.key
+        )
+        if (duplicatedDep) {
+          log.verbose('查询到重复依赖：', duplicatedDep)
+        } else {
+          // 需要合并到targetPkgPath的key
+          finalDep.push(templateDep)
+        }
+      })
+      return finalDep
+    }
   }
 
   async getPageTemplate() {
